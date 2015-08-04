23ad541ac09ce45596cdecbd1fe08ee20915bcf5
Petr Gazarov <petrgazarov@gmail.com>
2015-08-04 14:22:24 -0400

    refactor so that SlidingPiece Class DELTAS are generated in branches.

--- sliding_piece.rb
+++ sliding_piece.rb
@@ -4,17 +4,23 @@ class SlidingPiece < Piece
   DELTAS_HORIZ_VERT = SlidingPiece.generate_horiz_vert
 
   def self.generate_digonals
-    result = []
+    result = [[],[],[],[]]
     1.upto(Board::SIZE - 1) do |idx|
-      result += [[idx, idx], [idx, -idx], [-idx, idx], [-idx, -idx]]
+      result[0] << [idx, idx]
+      result[1] << [idx, -idx]
+      result[2] << [-idx, idx]
+      result[3] << [-idx, -idx]
     end
     result
   end
 
   def self.generate_horiz_vert
-    result = []
+    result = [[],[],[],[]]
     1.upto(Board::SIZE - 1) do |idx|
-      result += [[idx, 0], [-idx, 0], [0, idx], [0, -idx]]
+      result[0] << [idx, 0]
+      result[1] << [-idx, 0]
+      result[2] << [0, idx]
+      result[3] << [0, -idx]
     end
     result
   end
@@ -26,5 +32,14 @@ class SlidingPiece < Piece
     end
   end
 
+  def occupied?(pos)
+    board[pos]
+  end
+
+  private
+
+  def check_branch
+
+  end
 
 end
