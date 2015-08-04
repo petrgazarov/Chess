522ca09b1d86a40449744ee0f605668c397ab112
Petr Gazarov <petrgazarov@gmail.com>
2015-08-04 11:38:47 -0400

    Add [] methods to Board.

--- board.rb
+++ board.rb
@@ -6,4 +6,20 @@ class Board
   def initialize
     @grid = Array.new(SIZE) { Array.new(SIZE) }
   end
+
+  def in_check?(color)
+
+  end
+
+  def move(start_pos, end_pos)
+
+  end
+
+  def [](pos)
+    grid[pos[0], pos[1]]
+  end
+
+  def []=(pos, value)
+    self.grid[pos[0], pos[1]] = value
+  end
 end
