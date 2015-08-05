909978b3ebabb554593b0ac4463d039b1d27f17f
Petr Gazarov <petrgazarov@gmail.com>
2015-08-05 11:11:52 -0400

    fix bishop symbol

--- bishop.rb
+++ bishop.rb
@@ -6,9 +6,9 @@ class Bishop < SlidingPiece
 
   def to_s
     if color == :black
-      "\u265C".encode('utf-8')
+      "\u265D".encode('utf-8')
     else
-      "\u2656".encode('utf-8')
+      "\u2657".encode('utf-8')
     end
   end
 end
