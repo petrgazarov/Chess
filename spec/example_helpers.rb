d53f16a1cae9e8f3a79319fe2f786a25fc718b6b
Petr Gazarov <petrgazarov@gmail.com>
2015-10-21 16:53:59 -0400

    write king specs

--- spec/example_helpers.rb
+++ spec/example_helpers.rb
@@ -1,4 +1,6 @@
 module ExampleHelpers
+  # Below possible moves are for a white piece.
+  # Assume piece position [3, 3] unless noted otherwise.
   def bishop_empty_board_positions
     [[0, 0], [1, 1], [2, 2], [4, 4], [5, 5], [6, 6], [7, 7],
      [0, 6], [1, 5], [2, 4], [4, 2], [5, 1], [6, 0]]
@@ -8,12 +10,21 @@ module ExampleHelpers
     [[2, 2], [4, 4], [5, 5], [1, 5], [2, 4], [4, 2], [5, 1], [1, 1]]
   end
 
+  def king_empty_board_positions
+    [[2, 2], [2, 3], [2, 4], [3, 2], [3, 4], [4, 2], [4, 3], [4, 4]]
+  end
+  # piece position [5, 2]
+  def king_initial_setup_board_positions
+    [[5, 1], [4, 1], [4, 2], [4, 3], [5, 3]]
+  end
+
   def set_up_allow_for_piece(subject_piece, board, black_piece, white_piece)
     allow(subject_piece).to receive(:occupied?) do |pos|
-      occupied = [[1, 1], [1, 5], [1, 3], [1, 2], [1, 4], [6, 0], [6, 6], [6, 3]]
+      occupied = [
+        [1, 1], [1, 5], [1, 3], [1, 2], [1, 4], [6, 0], [6, 6], [6, 3], [6, 1], [6, 2]
+      ]
       occupied.include?(pos) ? true : false
     end
-
 
     allow(black_piece).to receive(:same_color?) { |color| color == :black }
     allow(white_piece).to receive(:same_color?) { |color| color == :white }
@@ -21,7 +32,7 @@ module ExampleHelpers
     allow(board).to receive(:[]) do |pos|
       if [[1, 1], [1, 5], [1, 3], [1, 2], [1, 4]].include?(pos)
         black_piece
-      elsif [[6, 0], [6, 6], [6, 3]].include?(pos)
+      elsif [[6, 0], [6, 6], [6, 3], [6, 1], [6, 2]].include?(pos)
         white_piece
       end
     end
--- spec/spec_files/pieces/king_spec.rb
+++ spec/spec_files/pieces/king_spec.rb
@@ -1,3 +1,38 @@
 require 'play'
 require 'example_group_helpers'
 require 'example_helpers'
+
+RSpec.describe King do
+  extend ExampleGroupHelpers
+
+  include_examples_for_piece_tests
+
+  let(:empty_board_king) { King.new([3, 3], empty_board, :white) }
+  let(:initial_board_king) { King.new([5, 2], initial_board, :white) }
+
+  describe "#moves" do
+    context "when position is [3, 3] on empty board" do
+      let(:moves) { king_empty_board_positions }
+
+      it "returns correct possible moves" do
+        extend ExampleHelpers
+
+        allow(empty_board_king).to receive(:occupied?).and_return false
+        expect(empty_board_king.moves.sort).to eq(moves.sort)
+      end
+    end
+
+    context "when position is [5, 2] on board with initial setup" do
+      let(:moves) { king_initial_setup_board_positions }
+
+      it "returns correct possible moves" do
+        extend ExampleHelpers
+
+        set_up_allow_for_piece(
+          initial_board_king, initial_board, black_piece, white_piece
+        )
+        expect(initial_board_king.moves.sort).to eq(moves.sort)
+      end
+    end
+  end
+end
