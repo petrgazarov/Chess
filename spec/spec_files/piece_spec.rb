0a7831290ef763e202e36b633b4a980ed8b2d680
Petr Gazarov <petrgazarov@gmail.com>
2015-10-22 18:36:36 -0400

    write specs for Piece#dup

--- spec/spec_files/piece_spec.rb
+++ spec/spec_files/piece_spec.rb
@@ -3,19 +3,32 @@ require 'example_group_helpers'
 require 'example_helpers'
 
 RSpec.describe Piece do
+  before(:all) do
+    @board = double("board")
+    @piece = Piece.new([3, 3], @board, :black)
+  end
   describe "#initialize" do
     it "sets position, board and color as instance variables" do
-      board = double("board")
-      piece = Piece.new([3, 3], board, :white)
-
-      expect(piece.position).to eq([3, 3])
-      expect(piece.board).to eq(board)
-      expect(piece.color).to eq(:white)
+      expect(@piece.position).to eq([3, 3])
+      expect(@piece.board).to eq(@board)
+      expect(@piece.color).to eq(:black)
     end
   end
 
   describe "#dup" do
-    it "returns a new instance of Piece"
+    before(:all) do
+      @new_board = double("board")
+      @dupped_piece = @piece.dup(@new_board)
+    end
+    it "returns a new instance of Piece" do
+      expect(@dupped_piece).not_to equal(@piece)
+    end
+
+    it "returns a piece with same color and position and with the given board" do
+      expect(@dupped_piece.color).to eq(@piece.color)
+      expect(@dupped_piece.position).to eq(@piece.position)
+      expect(@dupped_piece.board).to eq(@new_board)
+    end
   end
 
   describe "#same_color?" do
@@ -26,11 +39,5 @@ RSpec.describe Piece do
   describe "#occupied" do
     it "returns true if position is occupied"
     it "returns false if position is empty"
-  end
-
-  describe "#move_into_check?" do
-    it "returns true if move will result in a check"
-    it "returns false if move will not result in a check"
-    it "does not modify the board"
   end
 end
