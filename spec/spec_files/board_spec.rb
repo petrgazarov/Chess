6416c219eff61a7ae07a238f380eb9ee23da7d25
Petr Gazarov <petrgazarov@gmail.com>
2015-10-21 20:02:16 -0400

    write specs for Board::on_board?

--- spec/spec_files/board_spec.rb
+++ spec/spec_files/board_spec.rb
@@ -1,3 +1,16 @@
 require 'play'
 require 'example_group_helpers'
 require 'example_helpers'
+
+RSpec.describe Board do
+  describe "::on_board?" do
+    it "returns false when position is not on the board" do
+      expect(Board.on_board?([0, Board::SIZE + 1])).to eq false
+    end
+    it "returns true when position is on the board" do
+      expect(Board.on_board?([Board::SIZE - 1, 0])).to eq true
+    end
+  end
+
+
+end
